786a5e387321dc94225d2d7595834fdf5e6eec60
lealre <renaanvp@gmail.com>
2025-07-05 14:15:48 +0100

    Add response package in server

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"sync/atomic"
+
+	"github.com/lealre/httpfromtcp/internal/response"
 )
 
 // Server is an HTTP 1.1 server
@@ -49,9 +51,9 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	response := "HTTP/1.1 200 OK\r\n" +
-		"Content-Type: text/plain\r\n" +
-		"\r\n" +
-		"Hello World!\n"
-	conn.Write([]byte(response))
+
+	response.WriteStatusLine(conn, 200)
+	headers := response.GetDefaultHeaders(0)
+	response.WriteHeaders(conn, headers)
+	conn.Write([]byte("\r\n"))
 }
