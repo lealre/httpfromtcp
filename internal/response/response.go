f3a6f45cb439cb1f7903362bf5a5e75f66c856a3
lealre <renaanvp@gmail.com>
2025-07-05 14:15:25 +0100

    Add response package

---
+++ internal/response/response.go
@@ -0,0 +1,54 @@
+package response
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/lealre/httpfromtcp/internal/headers"
+)
+
+type StatusCode int
+
+const (
+	Ok                  StatusCode = 200
+	NotFound            StatusCode = 404
+	InternalServerError StatusCode = 500
+)
+
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	switch statusCode {
+	case 200:
+		_, err := w.Write([]byte("HTTP/1.1 200 OK\r\n"))
+		return err
+	case 400:
+		_, err := w.Write([]byte("HTTP/1.1 400 Bad Request\r\n"))
+		return err
+	case 500:
+		_, err := w.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n"))
+		return err
+	default:
+		return nil
+	}
+}
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	headers := headers.NewHeaders()
+
+	headers.Set("content-length", strconv.Itoa(contentLen))
+	headers.Set("connection", "close")
+	headers.Set("content-type", "text/plain")
+
+	return headers
+}
+
+func WriteHeaders(w io.Writer, headers headers.Headers) error {
+	for key, value := range headers {
+		keyPair := fmt.Sprintf("%s: %s\r\n", key, value)
+		_, err := w.Write([]byte(keyPair))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
